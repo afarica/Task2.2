6bb41aff1d23b99133648b60cb847fcc83859e00
afarica <57631090+afarica@users.noreply.github.com>
2019-11-12 02:40:29 +0600

    Update p22.py

--- p22.py
+++ p22.py
@@ -1,4 +1,8 @@
-
+# N students take K apples and distribute them among each other evenly. The
+# remaining (На английском языке что бы Вы научились понимать the undivisible) part remains in the basket. How many apples will
+# each single student get? How many apples will remain in the basket?
+# The program reads the numbers N and K. It should print the two answers for
+# the questions above. (Each N student will take K apples, and remains X)
 children=int(input("How many guys:"))
 apples=int(input("How many apples:"))
 c=str(children//apples)
